2297278b03015c161aad837e92da625a5c30cf0a
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-11-13 12:22:24 -0700

    Format code

--- app/Http/Controllers/api/StudentController.php
+++ app/Http/Controllers/api/StudentController.php
@@ -1,17 +1,18 @@
 <?php
 
 namespace App\Http\Controllers\api;
+
 use App\Exports\ExportStudent;
 use App\Http\Controllers\Controller;
+use App\Http\Filters\StudentFilter;
 use App\Imports\StudentsImport;
 use App\Models\Group;
 use App\Models\State;
+use App\Models\Student;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use App\Models\Student;
 use Maatwebsite\Excel\Facades\Excel;
 use PDF;
-use App\Http\Filters\StudentFilter;
 
 class StudentController extends Controller
 {
@@ -20,31 +21,33 @@ class StudentController extends Controller
         return Student::filter($filter)->with('center', 'address')->paginate(8);
     }
 
-       public function generate_excel(Request $request){
+    public function generate_excel(Request $request)
+    {
         $students = Student::with('center:id,name', 'address')
-        ->when($request->has('center_id'), function($q) use($request){
+        ->when($request->has('center_id'), function ($q) use ($request) {
             $q->where('center_id', $request->center_id);
         })
-        ->when($request->has('academic_level'), function($q) use($request){
+        ->when($request->has('academic_level'), function ($q) use ($request) {
             $q->where('academic_level', $request->academic_level);
         })
-        ->when($request->has('job_condition'), function($q) use($request){
+        ->when($request->has('job_condition'), function ($q) use ($request) {
             $q->where('job_condition', $request->job_condition);
         })
-         ->when($request->has('city'), function($q) use($request){
-            $q->whereHas('address', function($q) use($request){
+        ->when($request->has('city'), function ($q) use ($request) {
+            $q->whereHas('address', function ($q) use ($request) {
                 $q->where('city_id', $request->city);
             });
         })
         ->get();
-        foreach($students as $student){
+        foreach ($students as $student) {
             $student->birth_place = State::find($student->birth_place)->name ?? null;
         }
-//return $students;
-     return Excel::download(new ExportStudent($students), 'capacitandos.xlsx');
+        //return $students;
+        return Excel::download(new ExportStudent($students), 'capacitandos.xlsx');
     }
 
-    public function excel_import(Request $request){
+    public function excel_import(Request $request)
+    {
         $import = new StudentsImport();
         $file = $request->file('capacitandos')->store('imports');
         // $file = Storage::path('capacitandos (10).xlsx');
@@ -55,81 +58,88 @@ class StudentController extends Controller
         $info->put('errors', $import->failures());
         $info->put('new_records', $import->new_records);
 
-        if($import->failures()->isEmpty()){
+        if ($import->failures()->isEmpty()) {
             return response()->json([
-                'message' => "Registros insertados correctamente",
+                'message' => 'Registros insertados correctamente',
                 'code' => 2,
                 'data' => $info,
-                'errors' => null
+                'errors' => null,
             ], 200);
         }
 
         return response()->json([
-            'message' => "Ha ocurrido un error",
+            'message' => 'Ha ocurrido un error',
             'code' => -2,
             'data' => $info,
-            'errors' => null
+            'errors' => null,
         ], 200);
     }
 
-    public function reactivate(Request $request){
+    public function reactivate(Request $request)
+    {
         $student = Student::onlyTrashed()->where('curp', $request->curp)->first();
-        if($student){
+        if ($student) {
             $student->restore();
-            return response()->json([
-                    'message' => "Registro recuperado correctamente",
-                    'code' => 2,
-                    'data' => $student->id,
-                    'errors' => null
-                ], 200);
-        }
-        return response()->json([
-            'message' => "Ha ocurrido un error",
-            'code' => -2,
-            'data' => $request->curp,
-            'errors' => null
-        ], 200);
-    }
 
-    public function change_group_status(Request $request){
-        $group = Group::find($request->group_id);
-        if($group->students->contains('id', $request->student_id)){
-
-            $group->students()->sync([$request->student_id => ['status' => $request->status]], false);
             return response()->json([
-                'message' => "Registro editado correctamente",
+                'message' => 'Registro recuperado correctamente',
                 'code' => 2,
-                'data' => ['status' => $request->status, 'group_id' =>  $request->group_id, 'status' => $request->status],
-                'errors' => null
+                'data' => $student->id,
+                'errors' => null,
             ], 200);
         }
 
         return response()->json([
-            'message' => "Ha ocurrido un error",
+            'message' => 'Ha ocurrido un error',
             'code' => -2,
-            'data' => null,
-            'errors' => null
+            'data' => $request->curp,
+            'errors' => null,
         ], 200);
     }
-    public function generate_constancy(Request $request){
+
+    public function change_group_status(Request $request)
+    {
+        $group = Group::find($request->group_id);
+        if ($group->students->contains('id', $request->student_id)) {
+            $group->students()->sync([$request->student_id => ['status' => $request->status]], false);
+
+            return response()->json([
+                'message' => 'Registro editado correctamente',
+                'code' => 2,
+                'data' => ['status' => $request->status, 'group_id' => $request->group_id, 'status' => $request->status],
+                'errors' => null,
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => 'Ha ocurrido un error',
+            'code' => -2,
+            'data' => null,
+            'errors' => null,
+        ], 200);
+    }
+
+    public function generate_constancy(Request $request)
+    {
         $student = Student::find($request->student_id);
 
-        $group = ($student)? $student->groups->find($request->group_id) : null;
+        $group = ($student) ? $student->groups->find($request->group_id) : null;
 
         $year = Carbon::now()->format('Y');
         $month = Carbon::now()->format('m');
         $day = Carbon::now()->format('d');
 
-        if($group){
+        if ($group) {
             $group->load(['course:id,name,duration_course,type_course', 'course.themes:course_id,name', 'center:id,name,address_id', 'center.address.city.state:id,name']);
             // dd($group);
-         return PDF::loadView('admin.students.exports.constancia', compact('student', 'group', 'day', 'month', 'year'))->download("constancia_".time().".pdf");
+            return PDF::loadView('admin.students.exports.constancia', compact('student', 'group', 'day', 'month', 'year'))->download('constancia_'.time().'.pdf');
         }
+
         return response()->json([
-            'message' => "Ha ocurrido un error",
+            'message' => 'Ha ocurrido un error',
             'code' => -2,
             'data' => [],
-            'errors' => null
+            'errors' => null,
         ], 400);
     }
 }
