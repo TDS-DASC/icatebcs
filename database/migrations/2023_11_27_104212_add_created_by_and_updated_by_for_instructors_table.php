50b914fa2c882a90c28ce70149b8aeaafea8773d
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-11-28 11:04:17 -0700

    Add created_author and update_author for instructors

---
+++ database/migrations/2023_11_27_104212_add_created_by_and_updated_by_for_instructors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Instructor;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('instructors', function(Blueprint $table) {
+            $table->foreignIdFor(Instructor::class, 'created_by')->nullable();
+            $table->foreignIdFor(Instructor::class, 'updated_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('instructors', function(Blueprint $table) {
+            $table->dropColumn('created_by');
+            $table->dropColumn('updated_by');
+        });
+    }
+};
